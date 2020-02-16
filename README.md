35d3a84f00bf1508a0a473af09bf32ef1dc6a15b
Garlicvideos <Garlicvideos@gmail.com>
2020-02-16 11:10:48 +0800

    files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # sg-radio-bot
- 
+
+This is a discord bot that relays radio feed from radio stations in Singapore into any Discord voice channel.
+It currently isn't designed to work outside of my use case right now, but I will modify the code in the future so that other people can use it in a more practical manner.
+
+As such, I will not be providing any intructions on how to set this bot up as of yet. But, if you do have some experience in coding, it should be simple enough for you.
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Authors
+
+* **Garlicvideos** - [Garlicvideos](https://github.com/Garlicvideos)
+
+See also the list of [contributors](https://github.com/Garlicvideos/reddits-nightmare/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the GNU GENERAL PUBLIC LICENSE - see the [LICENSE](LICENSE) file for details
+
+## If you like what I do, please consider keeping me alive
+
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](http://paypal.me/Garlicvideos)
---
+++ bot/bot.js
@@ -0,0 +1,144 @@
+//Load required modules
+var discord = require("discord.js");
+var client = new discord.Client();
+var https = require("https");
+var convert = require('xml-js');
+var config = require("./config.json");
+var moment = require("moment");
+
+//Kiss92 stream URL
+var radioStream = "http://playerservices.streamtheworld.com/api/livestream-redirect/KISS_92AAC.aac?tdtok=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6ImZTeXA4In0.eyJpc3MiOiJ0aXNydiIsInN1YiI6IjIxMDY0IiwiaWF0IjoxNTgxNTE0MTQ4LCJ0ZC1yZWciOmZhbHNlfQ.k8L5xHd8S01FJ9a9QVxd8A48Eli9O_V9N89aiKkli48";
+var infoStream = "https://np.tritondigital.com/public/nowplaying?mountName=KISS_92AAC&numberToFetch=1&eventType=track&request.preventCache=1581742131922";
+
+//Boot discord bot
+client.on("ready", () => {
+	//Logged in to bot account
+	console.log(`Logged in as ${client.user.tag}`);
+});
+
+//Pretty print song duration
+function str_pad_left(string,pad,length) {
+    return (new Array(length+1).join(pad)+string).slice(-length);
+}
+
+//Starts the radio stream
+function playStream(message) {
+	//Find the voice channel
+	let channel = client.channels.get("658685417956179977");
+	//Join the voice channel
+	channel.join().then(connection => {
+		message.channel.send("Connected to voice channel!");
+		//Create broadcast
+		let broadcast = client.createVoiceBroadcast();
+		//Get stream from Kiss92
+		broadcast.playArbitraryInput(radioStream);
+		//Broadcast to every VC the bot is connected to
+		for (const connection of client.voiceConnections.values()) {
+		  	connection.playBroadcast(broadcast);
+		}
+	});
+}
+
+//Ends the radio stream
+function endStream(message) {
+	//Find the voice channel
+	let channel = client.channels.get("658685417956179977");
+	//Leave the voice channel
+	channel.leave();
+	message.channel.send("Radio stream stopped.");
+}
+
+//Get song info
+function getSongInfo(message) {
+	return new Promise((resolve, reject) => {
+		console.log("hello");
+		//Send http request to Kiss92
+		https.get(infoStream, function(res) {
+			res.on('data', (data) => {
+				//Set options for parser
+				let options = {
+					compact: true,
+					ignoreDoctype: true,
+					attributesKey: "attributes"
+				};
+				//Parse into JSON
+				let result = JSON.parse(convert.xml2json(data, options));
+				//Read JSON
+				let songTitle = result["nowplaying-info-list"]["nowplaying-info"]["property"][2]["_cdata"];
+				let songArtist = result["nowplaying-info-list"]["nowplaying-info"]["property"][3]["_cdata"];
+				let songStartTime = result["nowplaying-info-list"]["nowplaying-info"]["property"][1]["_cdata"];
+				let songDuration = result["nowplaying-info-list"]["nowplaying-info"]["property"][0]["_cdata"];
+				//Convert Song duration from Unix Timestamp to human readable format
+				songDuration = songStartTime - songDuration;
+				songDuration = moment.unix(songDuration/1000);
+				//Convert Song start time from Unix Timestamp to human readable format
+				let songStartMoment = moment.unix(songStartTime/1000);
+				songStartTime = songStartMoment.format('h:mm:ssa');
+				//Calculate Song duration
+				let duration = moment.duration(songStartMoment.diff(songDuration)).as('seconds');
+				let minutes = Math.floor(duration / 60);
+				let seconds = duration - minutes * 60;
+				songDuration = str_pad_left(minutes,'0',2)+':'+str_pad_left(seconds,'0',2);
+				console.log(songDuration);
+				//Send song info to chat
+				message.channel.send({embed: {
+					color: 3447003,
+					author: {
+						name: client.user.username,
+						icon_url: client.user.avatarURL
+					},
+					title: "Kiss92",
+					description: "Song Info",
+					fields: [{
+						name: "Song Title",
+						value: songTitle,
+						inline: true
+					},
+					{
+						name: "Artist",
+						value: songArtist,
+						inline: true
+					},
+					{
+						name: "Start time",
+						value: songStartTime,
+						inline: true
+					},
+					{
+						name: "Song Duration",
+						value: songDuration,
+						inline: true
+					}],
+					footer: {
+				      icon_url: client.user.avatarURL,
+				      text: "Info taken from Kiss92's API"
+				    }
+				}});
+				resolve();
+			});
+		});
+	});
+}
+
+//Bot commands
+client.on("message", async message => {
+	//Check if the message is a command
+	if (message.content.startsWith("!")) {
+		//Convert to lower case
+		message.content = message.content.toLowerCase();
+		let command = message.content;
+		if (command === "!play") {
+			//Initiate Kiss92 stream
+			playStream(message);
+		} else if (command === "!stop") {
+			//Disconnect from the voice channel
+			endStream(message);
+		} else if (command === "!song") {
+			//Get song info
+			await getSongInfo(message);
+		}
+	}
+});
+
+//Login to discord
+client.login(config.token);
